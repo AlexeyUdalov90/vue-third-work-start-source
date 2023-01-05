7d51f95befa1ae56bffd7d88c43d88655c6fc3ec
Aleksey Udalov <armiman69@gmail.com>
2023-01-05 13:04:03 +0300

    Добавляем layout

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app_layout">
+    <app-layout-header/>
+    <slot/>
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from '@/layouts/AppLayoutHeader.vue'
+export default {
+  name: 'AppLayout',
+  components: {
+    AppLayoutHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
